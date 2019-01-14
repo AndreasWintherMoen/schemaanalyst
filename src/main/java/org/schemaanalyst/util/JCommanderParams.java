12da1302f26d7cf7a6ed88393f90be1db706a2cd
Abdullah Alsharif <gxr199@gmail.com>
2019-01-14 15:41:26 +0000

    Added test case reduction commad in command params

--- src/main/java/org/schemaanalyst/util/JCommanderParams.java
+++ src/main/java/org/schemaanalyst/util/JCommanderParams.java
@@ -24,11 +24,14 @@ public class JCommanderParams {
   @Parameter(names = {"--seed","-rs","--randomseed"}, description = "A long random seed")
   protected long randomseed = -0L;
   
-  @Parameter(names = {"--reduce"}, description = "Post generation test suite reduction. Options: none (default), eqltc (Equal Test Cases), eqltr (Equal Test Requirements)")
+  @Parameter(names = {"--reduce"}, description = "Post generation test suite reduction. Options: none (default), eqltc (Equal Test Cases), eqltr (Equal Test Requirements), reduceTC (Reduce Test Cases INSERTS)")
   protected String reduce = "none";
   
   @Parameter(names = {"--reduceP","-rp","--reducePredicates"}, description = "If added it will reduce the generated predicates generated for each test requirments")
   protected boolean reducePredicates = false;
+  
+  @Parameter(names = {"--fullreduce","-fr"}, description = "Full reduce reduces test requirements, test case statements, equal test requirements and equal test cases. Default is deactivated")
+  protected boolean fullreduce = false;
   
   @Parameter(names = {"--printTR","-ptr","--printTestRequriments"}, description = "Print Test Requriments")
   protected boolean printTR = false;
