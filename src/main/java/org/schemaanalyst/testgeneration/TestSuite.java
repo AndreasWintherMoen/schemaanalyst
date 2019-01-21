c3a969a0df96ed5c3ee720a9bc76254567cc312e
Abdullah Alsharif <gxr199@gmail.com>
2019-01-21 15:42:34 +0000

    Added Counters of inserts into testsuite class

--- src/main/java/org/schemaanalyst/testgeneration/TestSuite.java
+++ src/main/java/org/schemaanalyst/testgeneration/TestSuite.java
@@ -12,6 +12,8 @@ public class TestSuite implements Serializable {
     private static final long serialVersionUID = -7221865547415541154L;
 
     private List<TestCase> testCases;
+    private int generatedInserts = 0;
+	private int reducedInsertsCount = 0;
 
     public TestSuite() {
         testCases = new ArrayList<>();
@@ -28,4 +30,31 @@ public class TestSuite implements Serializable {
     public List<TestCase> getTestCases() {
         return new ArrayList<>(testCases);
     }
+
+	public int getGeneratedInserts() {
+		return generatedInserts;
+	}
+
+	public void addGeneratedInserts(int originalInsertsCount) {
+		this.generatedInserts = originalInsertsCount + this.generatedInserts;
+	}
+
+	public int getReducedInsertsCount() {
+		return reducedInsertsCount;
+	}
+
+	public void addReducedInsertsCount(int reducedInsertsCount) {
+		this.reducedInsertsCount = reducedInsertsCount + reducedInsertsCount;
+	}
+	
+	public int countNumberOfInserts() {
+		int counter = 0;
+		
+		for (TestCase tc : testCases) {
+			counter = counter + tc.getData().getNumRows() + tc.getState().getNumRows();
+		}
+		
+		return counter;
+	}
+
 }
